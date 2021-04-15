27619915cb29cff84460d6e67c93723b146b9471
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-15 17:18:03 -0300

    Routes, user model, schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :v1 do
+    resources :registrations, only: [:create]
+    resources :sessions, only: [:create]
+  end
+
+  
 end
