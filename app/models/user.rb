3366ae8748f9a7c69bc5aff4cec82ef0e04c9bda
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-23 15:28:39 -0300

    Ran rubocop on app/models/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
-  enum gender: {male:0, female:1, other:2}
+  enum gender: { male: 0, female: 1, other: 2 }
 end
