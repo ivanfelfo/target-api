31fd4ef735fa2c1414d0a2f5d344a86bf5d09c91
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-23 19:11:42 -0300

    Enhanced create_spec

--- spec/requests/users/create_spec.rb
+++ spec/requests/users/create_spec.rb
@@ -3,20 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe 'POST /v1/users', type: :request do
+  subject { post v1_user_registration_path, params: params, as: :json }
   let(:params) { { user: { email: 'mail@mail.com', password: 'hello123', gender: true } } }
-  it 'saves user in db' do
-    post v1_user_registration_path, params: params, as: :json
-    user = User.last
-    expect(user.email).to eq('mail@mail.com')
-    expect(user.gender).to eq(true)
+
+  context 'when valid' do
+    it 'saves user in db' do
+      subject
+      user = User.last
+      expect(user.email).to eq('mail@mail.com')
+      expect(user.gender).to eq(true)
+    end
+
+    it 'responds with 200' do
+      subject
+      expect(response).to have_http_status(201)
+    end
   end
 
-  context 'when trying to register a user with an email that already exists' do
-    let!(:created_user) { User.create(email: 'mail@mail.com', password: 'hello123', gender: true) }
+  context 'when not valid' do
+    context 'when trying to register a user with an email that already exists' do
+      let!(:created_user) do
+        User.create(email: 'mail@mail.com', password: 'hello123', gender: true)
+      end
 
-    it 'does not create a new user' do
-      post v1_user_registration_path, params: params, as: :json
-      expect(User.count).to eq(1)
+      it 'does not create a new user' do
+        subject
+        expect(User.count).to eq(1)
+      end
+
+      it 'responds with 400' do
+        subject
+        expect(response).to have_http_status(400)
+      end
     end
   end
 end
