940f324bc3375d2be3c83c920e9fdcbded976060
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-26 13:07:45 -0300

    Fixed issues based on PR comments

--- spec/requests/sessions/create_spec.rb
+++ spec/requests/sessions/create_spec.rb
@@ -2,7 +2,10 @@
 
 require 'rails_helper'
 RSpec.describe 'POST /v1/users/sign_in', type: :request do
-  subject { post v1_user_session_path, params: params, as: :json }
+  subject do 
+    post v1_user_session_path, params: params, as: :json
+    response 
+  end
   let(:params) { { user: { email: 'mail@mail.com', password: 'hello123' } } }
 
   context 'when user already exists' do
@@ -10,13 +13,11 @@ RSpec.describe 'POST /v1/users/sign_in', type: :request do
 
     context 'when password matches' do
       it 'responds with code 200' do
-        subject
-        expect(response).to have_http_status(201)
+        expect(subject).to have_http_status(201)
       end
 
       it 'returns user email' do
-        subject
-        expect(JSON.parse(response.body)['email']).to eq(created_user.email)
+        expect(JSON.parse(subject.body)['email']).to eq(created_user.email)
       end
     end
 
@@ -24,17 +25,16 @@ RSpec.describe 'POST /v1/users/sign_in', type: :request do
       let(:params) { { user: { email: 'mail@mail.com', password: 'x' } } }
 
       it 'responds with code 401' do
-        subject
-        expect(response).to have_http_status(401)
+        expect(subject).to have_http_status(401)
       end
     end
 
     context 'when leaving a blank parameter' do
-      let(:params) { { user: { email: 'mail@mail.com' } } }
+      let(:params) { { user: { email: 'mail@mail.com', password: ''} } }
 
-      #   it  'responds with code 401' do
-      #     expect(response).to have_http_status(401)
-      #   end /// it is returning nil
+      it  'responds with code 401' do
+        expect(subject).to have_http_status(401)
+      end
     end
   end
 
@@ -42,8 +42,7 @@ RSpec.describe 'POST /v1/users/sign_in', type: :request do
     let(:params) { { user: { email: 'user_non_existent@mail.com', password: 'x' } } }
 
     it 'responds with code 401' do
-      subject
-      expect(response).to have_http_status(401)
+      expect(subject).to have_http_status(401)
     end
   end
 end
