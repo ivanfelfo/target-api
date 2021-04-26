bb85837e008695f03768fa84f0a2522c371c0572
ivanfelfo <53795144+ivanfelfo@users.noreply.github.com>
2021-04-26 16:52:28 -0300

    Enhancement: changed gender data type (#4)
    * Enhancement:final setup
    
    * Feature: signup added
    
    * Feature: signin added
    
    * Routes, user model, schema
    
    * Enhancement: fixed code after pull request feedback
    
    * Enhancement: fixes from the pr
    
    * Enhancement: fixed User model after pr feedback
    
    * Enhancement: fixes after pr feedback
    
    * Feature:signin & logout
    
    * Fixing signup
    
    * Enhancement: changed gender data type
    
    * Ran rubocop on app/models/user
    
    * remove tmp files
    
    * Fixed issues based on PR comments
    
    * Fixed issues based on PR comments
    
    Co-authored-by: Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'works with enum' do
+    is_expected.to define_enum_for(:gender)
+      .with_values(male: 0, female: 1, other: 2, unknown: 3)
+      .backed_by_column_of_type(:integer)
+  end
 end
--- spec/requests/users/create_spec.rb
+++ spec/requests/users/create_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'POST /v1/users', type: :request do
   subject { post v1_user_registration_path, params: params, as: :json }
-  let(:params) { { user: { email: 'mail@mail.com', password: 'hello123', gender: true } } }
+  let(:params) { { user: { email: 'mail@mail.com', password: 'hello123', gender: :male } } }
 
   context 'when valid' do
-    it 'creates and saves user in db' do
+    it 'creates a user in db' do
       subject
       user = User.last
       expect(user.email).to eq('mail@mail.com')
-      expect(user.gender).to eq(true)
+      expect(user.gender).to eq('male')
     end
 
     it 'responds with 200' do
@@ -22,12 +22,32 @@ RSpec.describe 'POST /v1/users', type: :request do
     it 'will change the user count by 1' do
       expect { subject }.to change(User, :count).by(1)
     end
+
+    context 'when creating a female user' do
+      let(:params) { { user: { email: 'mail@mail.com', password: 'hello123', gender: :female } } }
+
+      it 'will save female user in db' do
+        subject
+        female_user = User.last
+        expect(female_user.gender).to eq('female')
+      end
+    end
+
+    context 'when creating a user passing a blank gender' do
+      let(:params) { { user: { email: 'mail@mail.com', password: 'hello123' } } }
+
+      it 'will create a user with an unknown gender' do
+        subject
+        unknown_gender_user = User.last
+        expect(unknown_gender_user.gender).to eq('unknown')
+      end
+    end
   end
 
   context 'when not valid' do
     context 'when trying to register a user with an email that already exists' do
       let!(:created_user) do
-        User.create(email: 'mail@mail.com', password: 'hello123', gender: true)
+        User.create(email: 'mail@mail.com', password: 'hello123', gender: :male)
       end
 
       it 'does not create a new user' do
@@ -42,6 +62,14 @@ RSpec.describe 'POST /v1/users', type: :request do
 
       it 'will NOT change the user count' do
         expect { subject }.not_to change(User, :count)
+      end
+    end
+
+    context 'when creating a user passing random value gender' do
+      let(:params) { { user: { email: 'mail@mail.com', password: 'hello123', gender: 'x' } } }
+
+      it 'will NOT create a user' do
+        expect { subject }.to raise_error(ArgumentError, /'x' is not a valid gender/)
       end
     end
   end
