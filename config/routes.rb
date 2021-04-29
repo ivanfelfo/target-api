64a81855a66c700b897de8798a465e423c4bb7d0
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-29 12:51:46 -0300

    First steps setting up devise-token-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    # mount_devise_token_auth_for 'User', at: '/v1/users', controllers: {
+    #   registrations: 'v1/registrations',
+    #   sessions: 'v1/sessions'
+    # }
     devise_for :users, controllers: {
       confirmations: 'v1/confirmations'
     }
