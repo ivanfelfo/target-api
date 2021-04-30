6144e2a281e0ef62bb5acbc261e2d437fbadf2f9
ivanfelfo <53795144+ivanfelfo@users.noreply.github.com>
2021-04-30 09:46:29 -0300

    Enhancement: factory_bot & faker setup (#9)
    Co-authored-by: Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>

---
+++ spec/factories/users_factories.rb
@@ -0,0 +1,8 @@
+# This will guess the User class
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(8) }
+    gender   { rand(0..3) }
+  end
+end
