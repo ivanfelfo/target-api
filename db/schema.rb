2368b7815b2036ece5d7106bbac4b6f71ad67ad7
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-04-30 14:59:40 -0300

    Feature: devise_token_auth added

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,10 @@ ActiveRecord::Schema.define(version: 2021_04_29_132547) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "gender", default: 3
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.boolean "allow_password_change", default: false
     t.json "tokens"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
