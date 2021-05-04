3275ad155eb87e1082e38a24fb7b5ab800274412
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-05-04 19:04:39 -0300

    Added model validations to target and topic

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,9 @@
 class Target < ApplicationRecord
   belongs_to :topic
   belongs_to :user
+
+  validates :title, presence: true
+  validates :radius, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
--- spec/models/target_spec.rb
+++ spec/models/target_spec.rb
@@ -1,4 +1,13 @@
 describe Target, type: :model do
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:latitude) }
+    it { is_expected.to validate_presence_of(:longitude) }
+    it { is_expected.to validate_presence_of(:radius) }
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :topic }
+  end
+
   context 'when is a valid instance' do
     subject { build(:target) }
 
@@ -7,7 +16,4 @@ describe Target, type: :model do
       is_expected.to be_valid
     end
   end
-
-  it { is_expected.to belong_to :user }
-  it { is_expected.to belong_to :topic }
 end
