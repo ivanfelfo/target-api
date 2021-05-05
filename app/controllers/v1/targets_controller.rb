2e13b192467210fd586abc0453e3580212d21a58
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-05-05 10:07:05 -0300

    First approach

--- app/controllers/v1/targets_controller.rb
+++ app/controllers/v1/targets_controller.rb
@@ -1,9 +1,12 @@
 module V1
   class TargetsController < ApplicationController
-    before_action :authenticate_v1_user!
+    # before_action :authenticate_v1_user!
+    before_action :check_limit, only: :create
 
     def create
       @target = current_v1_user.targets.create!(target_params)
+      byebug
+      render_create_success
     end
 
     def destroy
@@ -12,12 +15,22 @@ module V1
 
     private
 
+    def check_limit
+      if current_v1_user.targets.count >= Target::MAX_TARGETS_PER_USER
+        render json: { error: "error! target limit reached" }
+      end
+    end
+
     def target
       @target ||= current_v1_user.targets.find(params[:id])
     end
 
     def target_params
       params.require(:target).permit(:topic_id, :user_id, :title, :radius, :latitude, :longitude)
+    end
+
+    def render_create_success
+      render json: { target: @target }
     end
   end
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,6 @@
 class Target < ApplicationRecord
+  MAX_TARGETS_PER_USER = 3
+
   belongs_to :topic
   belongs_to :user
 
