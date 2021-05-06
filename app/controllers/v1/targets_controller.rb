2debd01d63adeeb4901bd81cb4f277cb9c4d4a80
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-05-06 14:11:00 -0300

    Improvements based on PR comments

--- app/controllers/v1/targets_controller.rb
+++ app/controllers/v1/targets_controller.rb
@@ -1,7 +1,6 @@
 module V1
   class TargetsController < ApplicationController
     before_action :authenticate_v1_user!
-    before_action :check_limit, only: :create
 
     def create
       @target = current_v1_user.targets.create!(target_params)
@@ -13,12 +12,6 @@ module V1
     end
 
     private
-
-    def check_limit
-      return unless current_v1_user.targets.count >= Target::MAX_TARGETS_PER_USER
-
-      render json: { error: 'Target limit reached' }, status: :bad_request
-    end
 
     def target
       @target ||= current_v1_user.targets.find(params[:id])
--- app/models/target.rb
+++ app/models/target.rb
@@ -8,4 +8,14 @@ class Target < ApplicationRecord
   validates :radius, presence: true, numericality: { greater_than: 0 }
   validates :latitude, presence: true, numericality: true
   validates :longitude, presence: true, numericality: true
+
+  validate :user_target_limit
+
+  private
+
+  def user_target_limit
+    return unless user.present? && user.targets.count >= Target::MAX_TARGETS_PER_USER
+
+    errors.add(:user, I18n.t('targets.create.limit_reached', limit: MAX_TARGETS_PER_USER))
+  end
 end
