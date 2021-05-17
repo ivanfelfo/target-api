707878dea9ee54cb4e5347be27688a1ed5ed0971
Ivan Feldfogel <ivanfelfo@Ivans-MacBook-Pro.local>
2021-05-17 16:47:53 -0300

    Created onesignal service

--- app/models/target.rb
+++ app/models/target.rb
@@ -23,33 +23,11 @@ class Target < ApplicationRecord
 
   def notify_compatible_targets
     compatible_users.each do |user|
-      included_targets = OneSignal::IncludedTargets.new(include_email_tokens: [user.email])
-      notification = OneSignal::Notification.new(headings: notification_headings,
-                                                 contents: notification_contents,
-                                                 included_targets: included_targets,
-                                                 email_subject: notification_email_subject,
-                                                 template_id: notification_template_id)
-      OneSignal.send_notification(notification)
+      OneSignalService.new(user).call
     end
   end
 
   private
-
-  def notification_headings
-    OneSignal::Notification::Headings.new(en: I18n.t('onesignal.notification.headings'))
-  end
-
-  def notification_contents
-    OneSignal::Notification::Contents.new(en: I18n.t('onesignal.notification.contents'))
-  end
-
-  def notification_email_subject
-    I18n.t('onesignal.notification.email_subject')
-  end
-
-  def notification_template_id
-    Rails.application.credentials.onesignal[:template_id]
-  end
 
   def compatible_users
     compatible_targets.map(&:user)
